0bb9e51b5d97b2bead2ebb0665919770ad1a1ccf
chenwu <chengyi.wu@outlook.com>
2018-10-12 18:34:15 +0800

    + fix addons

--- wowAddons.py
+++ wowAddons.py
@@ -6,7 +6,17 @@ import sockshandler
 
 addOns = {
     "Quartz" : "https://www.wowace.com/projects/quartz/files/latest",
-    "Deadly Boss Mods (DBM)" : "https://wow.curseforge.com/projects/deadly-boss-mods/files/latest"
+    "Deadly Boss Mods (DBM)" : "https://wow.curseforge.com/projects/deadly-boss-mods/files/latest",
+    "Bagnon" : "https://wow.curseforge.com/projects/bagnon/files/latest",
+    "CompactRaid" : "https://wow.curseforge.com/projects/compactraid/files/latest",
+    "Garrison Mission Manager" : "https://wow.curseforge.com/projects/garrison-mission-manager/files/latest",
+    "Immersion" : "https://wow.curseforge.com/projects/immersion/files/latest",
+    "Mapster" : "https://www.wowace.com/projects/mapster/files/latest",
+    "OmniCC" : "https://wow.curseforge.com/projects/omni-cc/files/latest",
+    "Postal" : "https://www.wowace.com/projects/postal/files/latest",
+    "Skada" : "https://www.wowace.com/projects/skada/files/latest",
+    "World Quests List" : "https://wow.curseforge.com/projects/world-quests-list/files/latest",
+    "Scrap (Junk Seller)" : "https://wow.curseforge.com/projects/scrap/files/latest"
 }
 
 useProxy = False
@@ -30,10 +40,10 @@ if __name__ == '__main__':
         proxy = sockshandler.SocksiPyHandler(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 1080)
         opener = urllib.request.build_opener(proxy)
         urllib.request.install_opener(opener)
-    folder = '.'
+    folder = 'D:/Games/World of Warcraft/Interface/AddOns'
     for k in addOns:
         url = addOns[k]
         print("Downloading %s from %s ..." % (k, url))
         file = dlAddOn(url)
         print("\nExtracting %s to %s ..." % (file, folder))
-        unZip(file)
+        unZip(file, folder)
