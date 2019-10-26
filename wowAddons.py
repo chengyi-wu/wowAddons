968ee3b280df03b1ee60fe5b8c8eb7473a8808a6
chengyimbp <chengyi.wu@outlook.com>
2019-10-26 09:56:34 +0800

    +Changes
    -cfscrape
    -addons.csv

--- wowAddons.py
+++ wowAddons.py
@@ -1,53 +1,95 @@
 import urllib.request
 import sys
 import zipfile
+import csv
+import os
+import time
+import cfscrape
 
 addOns = {
-    # "Deadly Boss Mods (DBM)" : "https://wow.curseforge.com/projects/deadly-boss-mods/files/latest",
-    "BigWigs Bossmods" : "https://wow.curseforge.com/projects/big-wigs/files/latest",
-    "LittleWigs" : "https://wow.curseforge.com/projects/little-wigs/files/latest",
-    "BigWigs_Voice" : "https://wow.curseforge.com/projects/bigwigs_voice/files/latest",
-    "Bigwigs Voice - Chinese voicepack (VV)" : "https://wow.curseforge.com/projects/bigwigs-voice-chinese-voicepack-vv/files/latest",
-    "Details! Damage Meter" : "https://wow.curseforge.com/projects/details/files/latest",
-    "Skada Damage Meter" : "https://www.wowace.com/projects/skada/files/latest",
-    "Quartz" : "https://www.wowace.com/projects/quartz/files/latest",
-    "Bagnon" : "https://wow.curseforge.com/projects/bagnon/files/latest",
-    "CompactRaid" : "https://wow.curseforge.com/projects/compactraid/files/latest",
-    "Immersion" : "https://wow.curseforge.com/projects/immersion/files/latest",
-    "Mapster" : "https://www.wowace.com/projects/mapster/files/latest",
-    "OmniCC" : "https://wow.curseforge.com/projects/omni-cc/files/latest",
-    "Postal" : "https://www.wowace.com/projects/postal/files/latest",
-    "KuiNameplates" : "https://wow.curseforge.com/projects/kuinameplates/files/latest",
-    "Garrison Mission Manager" : "https://wow.curseforge.com/projects/garrison-mission-manager/files/latest",
-    "World Quests List" : "https://wow.curseforge.com/projects/world-quests-list/files/latest",
-    "Scrap (Junk Seller)" : "https://wow.curseforge.com/projects/scrap/files/latest",
-    "TinyInspect" : "https://wow.curseforge.com/projects/itemlevel-anywhere/files/latest",
-    "Pa'ku Totems" : "https://wow.curseforge.com/projects/paku-totems/files/latest",
-    "BonusRollPreview" : "https://wow.curseforge.com/projects/bonusrollpreview/files/latest",
-    "KeystoneHelper" : "https://wow.curseforge.com/projects/keystonehelper/files/latest",
-    "Angry Keystones" : "https://www.wowace.com/projects/angry-keystones/files/latest", 
-    "Can I Mog It?" : "https://wow.curseforge.com/projects/can-i-mog-it/files/latest",
-    "alaChat" : "https://wow.curseforge.com/projects/alachat/files/latest",
-    "EnhancedChatFilterMODFix" : "https://wow.curseforge.com/projects/ecfmodfix/files/latest", 
-    "AzeriteTooltip" : "https://wow.curseforge.com/projects/azeritetooltip/files/latest",
-    "BFAInvasionTimer" : "https://wow.curseforge.com/projects/bfainvasiontimer/files/latest",
-    "Quest Completist" : "https://wow.curseforge.com/projects/quest_completist/files/latest",
-    "WeakAuras 2" : "https://www.wowace.com/projects/weakauras-2/files/latest",
-    "Shadowed Unit Frames" : "https://www.wowace.com/projects/shadowed-unit-frames/files/latest",
-    "Talent Set Manager" : "https://wow.curseforge.com/projects/talent-set-manager/files/latest",
-    "Nameplate Scrolling Combat Text" : "https://wow.curseforge.com/projects/nameplate-scrolling-combat-text/files/latest",
-    "AllTheIDs" : "https://wow.curseforge.com/projects/alltheids/files/latest",
-    "Instance Portals" : "https://wow.curseforge.com/projects/instance-portals/files/latest",
-    "Astral Keys" : "https://wow.curseforge.com/projects/astral-keys/files/latest",
+    "BigWigs Bossmods" : "https://www.curseforge.com/wow/addons/big-wigs/download",
+    "LittleWigs" : "https://www.curseforge.com/wow/addons/little-wigs/download",
+    "BigWigs_Voice" : "https://www.curseforge.com/wow/addons/bigwigs_voice/download/",
+    "Bigwigs Voice - Chinese voicepack (VV)" : "https://www.curseforge.com/wow/addons/bigwigs-voice-chinese-voicepack-vv/download/",
+    "Details! Damage Meter" : "https://www.curseforge.com/wow/addons/details/download/",
+    "Quartz" : "https://www.curseforge.com/wow/addons/quartz/download/",
+    "Bagnon" : "https://www.curseforge.com/wow/addons/bagnon/download/",
+    # "CompactRaid" : "https://www.curseforge.com/wow/addons/compactraid/download/",
+    "Immersion" : "https://www.curseforge.com/wow/addons/immersion/download/",
+    "Mapster" : "https://www.curseforge.com/wow/addons/mapster/download",
+    "OmniCC" : "https://www.curseforge.com/wow/addons/omni-cc/download/",
+    "Postal" : "https://www.curseforge.com/wow/addons/postal/download",
+    "KuiNameplates" : "https://www.curseforge.com/wow/addons/kuinameplates/download/",
+    "Garrison Mission Manager" : "https://www.curseforge.com/wow/addons/garrison-mission-manager/download",
+    "World Quests List" : "https://www.curseforge.com/wow/addons/world-quests-list/download",
+    "Scrap (Junk Seller)" : "https://www.curseforge.com/wow/addons/scrap/download/",
+    "TinyInspect" : "https://www.curseforge.com/wow/addons/itemlevel-anywhere/download/",
+    "BonusRollPreview" : "https://www.curseforge.com/wow/addons/bonusrollpreview/download/",
+    "KeystoneHelper" : "https://www.curseforge.com/wow/addons/keystonehelper/download/",
+    "Angry Keystones" : "https://www.curseforge.com/wow/addons/angry-keystones/download", 
+    "Can I Mog It?" : "https://www.curseforge.com/wow/addons/can-i-mog-it/download/",
+    "alaChat" : "https://www.curseforge.com/wow/addons/alachat/download/",
+    "EnhancedChatFilterMODFix" : "https://www.curseforge.com/wow/addons/ecfmodfix/download/", 
+    "AzeriteTooltip" : "https://www.curseforge.com/wow/addons/azeritetooltip/download/",
+    # "BFAInvasionTimer" : "https://www.curseforge.com/wow/addons/bfainvasiontimer/download/",
+    "WeakAuras 2" : "https://www.curseforge.com/wow/addons/weakauras-2/download",
+    "Shadowed Unit Frames" : "https://www.curseforge.com/wow/addons/shadowed-unit-frames/download",
+    "Talent Set Manager" : "https://www.curseforge.com/wow/addons/talent-set-manager/download/",
+    "Nameplate Scrolling Combat Text" : "https://www.curseforge.com/wow/addons/nameplate-scrolling-combat-text/download/",
+    "AllTheIDs" : "https://www.curseforge.com/wow/addons/alltheids/download",
+    "Instance Portals" : "https://www.curseforge.com/wow/addons/instance-portals/download",
+    "Astral Keys" : "https://www.curseforge.com/wow/addons/astral-keys/download",
+    "BugGrabber" : "https://www.wowace.com/projects/bug-grabber/files/latest",
+    "BugSack" : "https://www.wowace.com/projects/bugsack/files/latest",
+    "MythicPlusTimer" : "https://www.curseforge.com/wow/addons/mythicplustimer/download", 
+    "LibGroupInSpecT" : "https://www.curseforge.com/wow/addons/libgroupinspect/download",
+    "LibGetFrame" : "https://www.curseforge.com/wow/addons/libgetframe/download/",
+    "SavedInstances" : "https://www.curseforge.com/wow/addons/saved_instances/download",
+    "AdvancedInterfaceOptions" : "https://www.curseforge.com/wow/addons/advancedinterfaceoptions/download",
+    "TinyTooltip" : "https://www.curseforge.com/wow/addons/tinytooltip/download",
+    "Auctionator" : "https://www.curseforge.com/wow/addons/auctionator/download",
+    # "Bartender4" : "https://www.wowace.com/projects/bartender4/files/latest",
+    "HandyNotes" : "https://www.curseforge.com/wow/addons/handynotes/download",
+    # "HandyNotes_DraenorTreasures" : "https://wow.curseforge.com/projects/handynotes_draenortreasures/files/latest",
+    "HandyNotes - Nazjatar by TomCat's Tours" : "https://www.curseforge.com/wow/addons/tomcats-tours-nazjatar/download",
+    "HandyNotes - Mechagon by TomCat's Tours" : "https://www.curseforge.com/wow/addons/tomcats-tours-mechagon/download",
+    "TomCat's Tours" : "https://www.curseforge.com/wow/addons/tomcats/download", 
 }
 
-def dlAddOn(url):
+scraper = cfscrape.create_scraper()
+def urlretrieve(url, proxies=None, reporthook=None):
+    response = scraper.get(url,  proxies=proxies)
+    filename = 'tmp'
+    with open(filename, 'bw') as f:
+        f.write(response.content)
+    return filename
+
+def parseCurseForge(file):
+    with open(file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if "If your download doesn't start automatically, click" in line:
+                start = line.index('"') + 1
+                end = line.index('"', start)
+                return "https://www.curseforge.com" + line[start : end]
+    return None
+
+def dlAddOn(name, url, proxies, force=False):
     def progresshook(chunk, chunkSize, totalSize):
         size = min(chunk * chunkSize, totalSize)
         sys.stdout.write("%d KB / %d KB\r" % (size / 1024, totalSize / 1024))
         sys.stdout.flush()
 
-    file, _ = urllib.request.urlretrieve(url, reporthook=progresshook)
+    # file, _ = urllib.request.urlretrieve(url, reporthook=progresshook)
+    file = urlretrieve(url, proxies=proxies, reporthook=progresshook)
+    if "www.curseforge.com" in url:
+        url = parseCurseForge(file)
+        print("[%s] -> [%s]" % (file, url))
+        os.remove(file)
+        file = None
+        if force or addOns[name]["VERSION"] != url:
+            file = urlretrieve(url, reporthook=progresshook)
+            addOns[name]["VERSION"] = url
 
     return file
 
@@ -56,25 +98,65 @@ def unZip(file, folder = '.'):
         f.extractall(folder)
 
 if __name__ == '__main__':
-    folder = 'D:/Games/World of Warcraft/_retail_/Interface/AddOns'
-    use_proxy = True
-    
-    proxy = urllib.request.ProxyHandler({'http': 'http://raspberrypi:8118', 'https': 'http://raspberrypi:8118'})
-    if use_proxy:
-        opener = urllib.request.build_opener(proxy)
+    if os.name == 'nt':
+        folder = 'D:/Games/World of Warcraft/_retail_/Interface/AddOns'
     else:
-        opener = urllib.request.build_opener()
+        folder = '/Applications/World of Warcraft/_retail_/Interface/AddOns'
+    use_proxy = True
+    file_name = "addons.csv"
 
-    opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0')]
-    urllib.request.install_opener(opener)
-    
+    proxies = {'http': 'http://raspberrypi:8118', 'https': 'http://raspberrypi:8118'}
+    proxies = None
+
+    # if use_proxy:
+    #     opener = urllib.request.build_opener(proxy)
+    # else:
+    #     opener = urllib.request.build_opener()
+
+    # urllib.request.install_opener(opener)
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    if not os.path.exists(file_name):
+        with open(file_name, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(["Name", "URL", "VERSION", "TIMESTAMP"])
+            for k in addOns:
+                url = addOns[k]
+                writer.writerow([k, url, '', ''])
+
+    with open(file_name, 'r') as f:
+        reader = csv.reader(f)
+        header = True
+        for row in reader:
+            if header:
+                header = False
+                addOns = {}
+            else:
+                k = row[0]
+                v = { "Name": row[0], "URL" : row[1], "VERSION": row[2], "TIMESTAMP" : row[3] }
+                addOns[k] = v             
+            
     for k in addOns:
         try:
-            url = addOns[k]
+            url = addOns[k]["URL"]
             print("Downloading [%s] from [%s] ..." % (k, url))
-            file = dlAddOn(url)
-            print("\nExtracting [%s] to [%s] ..." % (file, folder))
-            unZip(file, folder)
+            file = dlAddOn(k, url, proxies, force=True)
+            if file is None:
+                print('[%s] No updates' % (k))
+            else:
+                print("Extracting [%s] to [%s] ..." % (file, folder))
+                unZip(file, folder)
+                os.remove(file)
+            addOns[k]["TIMESTAMP"] = time.ctime()
         except Exception as err:
             print("Failed to get [%s]: %r" % (k, err))
+            addOns[k]["TIMESTAMP"] = "ERR"
+
+    with open(file_name, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Name", "URL", "VERSION", "TIMESTAMP"])
+        for k in addOns:
+            writer.writerow([addOns[k]["Name"], addOns[k]["URL"], addOns[k]["VERSION"], addOns[k]["TIMESTAMP"]])
             
